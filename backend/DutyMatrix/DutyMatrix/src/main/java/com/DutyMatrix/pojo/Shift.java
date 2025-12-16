ee2a54bbaa9b1d2ed7f06d24e140a4143d60f805
Akshat Verma <19.aks.v@gmail.com>
2025-12-16 23:42:56 +0530

    Swap request entity done

--- backend/DutyMatrix/DutyMatrix/src/main/java/com/DutyMatrix/pojo/Shift.java
+++ backend/DutyMatrix/DutyMatrix/src/main/java/com/DutyMatrix/pojo/Shift.java
@@ -2,10 +2,14 @@ package com.DutyMatrix.pojo;
 
 import java.util.Date;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -16,7 +20,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name= "Shift")
+@Table(name= "shifts")
 @NoArgsConstructor
 
 @Setter
@@ -24,10 +28,15 @@ import lombok.ToString;
 @ToString
 public class Shift {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="shift_id")
 	private int shid;
+	
 	@Enumerated(EnumType.STRING)
+	@Column(name = "shift_type")
 	private ShiftType shtype;
+	
 	@Column(name = "shift_startTime")
 	private Date shStartTime;
 	@Column(name = "shift_endTime")
---
+++ backend/DutyMatrix/DutyMatrix/src/main/java/com/DutyMatrix/pojo/SwapRequest.java
@@ -0,0 +1,56 @@
+package com.DutyMatrix.pojo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table(name = "swap_requests")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class SwapRequest {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "swap_id")
+	private int swapId;
+	
+	@Column(name = "reason")
+	private String reason;
+	
+//	@Enumerated(EnumType.STRING)
+//	@Column(name = "status")
+//	private RequestStatus status;
+	
+//	@ManyToOne
+//	@JoinColumn(name = "requesting_user_id")
+//	private User requestingUserId;
+	
+//	@ManyToOne
+//	@JoinColumn(name = "target_user_id")
+//	private User targetUserId;
+	
+	@ManyToOne
+	@JoinColumn(name = "shift_id", nullable = false)
+	private Shift shift;
+	
+//	@ManyToOne
+//	@JoinColumn(name = "approved_by")
+//	private User approvedBy;
+}
